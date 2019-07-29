d8de97ff68e02997aba499e76f8b88c080909015
jjone <jiwon.j306@gmail.com>
2019-07-29 20:03:10 +0900

    ReadMe: Add Reference

--- 03.YOLO/util.py
+++ 03.YOLO/util.py
@@ -26,31 +26,31 @@ def load_test_data(img_file):
     return input
 
 
-def predict_transform(prediction, im_size, anchors, n_classes, CUDA = True):
+def predict_transform(pred, im_size, anchors, n_classes, CUDA = True):
     """
     Converting the feature map from the detection layer to 2D tensor
-    Input -pred: the output feature map from the previous convolutional layer
-    -im_size: the dimension of input images (416*416)
-    -anchors: yolo priors
-    -n_classes: number of classes of the dataset (80)
+    Input  -pred: the output feature map from the previous convolutional layer
+           -im_size: the dimension of input images (416*416)
+           -anchors: yolo priors
+           -n_classes: number of classes of the dataset (80)
     Output -2D tensor of the predicion, (4 bbox coordinates + class prediction) for each of 3 boxes
     """
-    batch_size = prediction.size(0)
-    stride =  im_size // prediction.size(2)
+    batch_size = pred.size(0)
+    stride =  im_size // pred.size(2)
     grid_size = im_size // stride
     bbox_attrs = 5 + n_classes
     num_anchors = len(anchors)
 
     anchors = [(a[0]/stride, a[1]/stride) for a in anchors]
 
-    prediction = prediction.view(batch_size, bbox_attrs*num_anchors, grid_size*grid_size)
-    prediction = prediction.transpose(1,2).contiguous()
-    prediction = prediction.view(batch_size, grid_size*grid_size*num_anchors, bbox_attrs)
+    pred = pred.view(batch_size, bbox_attrs*num_anchors, grid_size*grid_size)
+    pred = pred.transpose(1,2).contiguous()
+    pred = pred.view(batch_size, grid_size*grid_size*num_anchors, bbox_attrs)
 
     #Sigmoid the  centre_X, centre_Y. and object confidencce
-    prediction[:,:,0] = torch.sigmoid(prediction[:,:,0])
-    prediction[:,:,1] = torch.sigmoid(prediction[:,:,1])
-    prediction[:,:,4] = torch.sigmoid(prediction[:,:,4])
+    pred[:,:,0] = torch.sigmoid(pred[:,:,0])
+    pred[:,:,1] = torch.sigmoid(pred[:,:,1])
+    pred[:,:,4] = torch.sigmoid(pred[:,:,4])
 
     #Add the center offsets
     grid_len = np.arange(grid_size)
@@ -65,7 +65,7 @@ def predict_transform(prediction, im_size, anchors, n_classes, CUDA = True):
 
     x_y_offset = torch.cat((x_offset, y_offset), 1).repeat(1,num_anchors).view(-1,2).unsqueeze(0)
 
-    prediction[:,:,:2] += x_y_offset
+    pred[:,:,:2] += x_y_offset
 
     #log space transform height and the width
     anchors = torch.FloatTensor(anchors)
@@ -74,15 +74,99 @@ def predict_transform(prediction, im_size, anchors, n_classes, CUDA = True):
         anchors = anchors.cuda()
 
     anchors = anchors.repeat(grid_size*grid_size, 1).unsqueeze(0)
-    prediction[:,:,2:4] = torch.exp(prediction[:,:,2:4])*anchors
+    pred[:,:,2:4] = torch.exp(pred[:,:,2:4])*anchors
 
     #Softmax the class scores
-    prediction[:,:,5: 5 + n_classes] = torch.sigmoid((prediction[:,:, 5 : 5 + n_classes]))
+    pred[:,:,5: 5 + n_classes] = torch.sigmoid((pred[:,:, 5 : 5 + n_classes]))
+    pred[:,:,:4] *= stride
 
-    prediction[:,:,:4] *= stride
-
-    return prediction
+    return pred
 
 
-def drawing_box():
-    pass
+def write_results(prediction, confidence, num_classes, nms = True, nms_conf = 0.4):
+    conf_mask = (prediction[:,:,4] > confidence).float().unsqueeze(2)
+    prediction = prediction*conf_mask
+
+    try:
+        ind_nz = torch.nonzero(prediction[:,:,4]).transpose(0,1).contiguous()
+    except:
+        return 0
+
+    box_a = prediction.new(prediction.shape)
+    box_a[:,:,0] = (prediction[:,:,0] - prediction[:,:,2]/2)
+    box_a[:,:,1] = (prediction[:,:,1] - prediction[:,:,3]/2)
+    box_a[:,:,2] = (prediction[:,:,0] + prediction[:,:,2]/2)
+    box_a[:,:,3] = (prediction[:,:,1] + prediction[:,:,3]/2)
+    prediction[:,:,:4] = box_a[:,:,:4]
+
+
+    batch_size = prediction.size(0)
+
+    output = prediction.new(1, prediction.size(2) + 1)
+    write = False
+
+    for ind in range(batch_size):
+        #select the image from the batch
+        image_pred = prediction[ind]
+
+        max_conf, max_conf_score = torch.max(image_pred[:,5:5+ num_classes], 1)
+        max_conf = max_conf.float().unsqueeze(1)
+        max_conf_score = max_conf_score.float().unsqueeze(1)
+        seq = (image_pred[:,:5], max_conf, max_conf_score)
+        image_pred = torch.cat(seq, 1)
+
+        #Get rid of the zero entries
+        non_zero_ind =  (torch.nonzero(image_pred[:,4]))
+
+
+        image_pred_ = image_pred[non_zero_ind.squeeze(),:].view(-1,7)
+
+        #Get the various classes detected in the image
+        try:
+            img_classes = unique(image_pred_[:,-1])
+        except:
+             continue
+        #WE will do NMS classwise
+        for cls in img_classes:
+            #get the detections with one particular class
+            cls_mask = image_pred_*(image_pred_[:,-1] == cls).float().unsqueeze(1)
+            class_mask_ind = torch.nonzero(cls_mask[:,-2]).squeeze()
+
+            image_pred_class = image_pred_[class_mask_ind].view(-1,7)
+
+            conf_sort_index = torch.sort(image_pred_class[:,4], descending = True )[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)
+
+            #if nms has to be done
+            if nms:
+                #For each detection
+                for i in range(idx):
+                    #Get the IOUs of all boxes that come after the one we are looking at
+                    #in the loop
+                    try:
+                        ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i+1:])
+                    except ValueError:
+                        break
+
+                    except IndexError:
+                        break
+
+                    #Zero out all the detections that have IoU > treshhold
+                    iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                    image_pred_class[i+1:] *= iou_mask
+
+                    #Remove the non-zero entries
+                    non_zero_ind = torch.nonzero(image_pred_class[:,4]).squeeze()
+                    image_pred_class = image_pred_class[non_zero_ind].view(-1,7)
+
+            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
+            seq = batch_ind, image_pred_class
+            if not write:
+                output = torch.cat(seq,1)
+                write = True
+            else:
+                out = torch.cat(seq,1)
+                output = torch.cat((output,out))
+
+    return output
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 <br>
 
-## ***1. Paper with Code***
+## ***1. Paper Reproduction***
 
 Studying neural networks can be divided into three parts: *The applications*, *the implementations*, and *intuitions behind these architectures*. Thanks to the user-friendly frameworks such Keras, the applications part are open to everyone. But grasping the real intuition behind the model is overlooked sometimes. What’s the researchers’ intention for building a model with such structures? What motivated them to take such an approach? And what can we infer from the outcome?  
 
@@ -22,7 +22,7 @@ This repository is an on-going project for studying the state-of-the-art network
 
 - Part1. [VGG, ResNet, Inception Network, Xception and MobileNet](https://towardsdatascience.com/deep-dive-into-the-computer-vision-world-f35cd7349e16?source=friends_link&sk=449ea5da20c884dadca23b907efb7e13)
 - Part2. [R-CNN, Fast R-CNN, and Faster R-CNN](https://towardsdatascience.com/deep-dive-into-the-computer-vision-world-part-2-7a24efdb1a14?source=friends_link&sk=4fec4dfc9499c930f263c6808b2f369d)
-- Part3. YOLO and SSD
+- Part3. [YOLO and SSD, Mask R-CNN]()
 
 <br>
 
@@ -33,4 +33,21 @@ This repository is an on-going project for studying the state-of-the-art network
 - [Inception-V1 in Keras](https://github.com/jjone36/dl_scratch/blob/master/02.InceptionNetwork/inception_v1_keras.py)
 - [Inception-V4 in Pytorch](https://github.com/jjone36/dl_scratch/blob/master/02.InceptionNetwork/inception_v4_pytorch.py)
 - [Inception-ResNet V2 in Tensorflow](https://github.com/jjone36/dl_scratch/blob/master/02.InceptionNetwork/inception-resnet_v2_tf.py)
-- [YOLO V3 in PyTorch]()
+- [YOLO V3 in PyTorch](https://github.com/jjone36/dl_scratch/tree/master/03.YOLO)
+
+<br>
+
+## ***4. Reference***
+- Karen Simonyan and Andrew Zisserman, [Very deep convolutional network for large-scale image recognition](https://arxiv.org/abs/1409.1556), 2015
+- Kaiming He, et al. [Deep residual learning for image recognition](https://arxiv.org/abs/1512.03385), 2015
+- Christian Szegedy, et al. [Going deeper with convolutions](https://arxiv.org/abs/1409.4842), 2014
+- Christian Szegedy et al., [Inception-v4, Inception-ResNet and the Impact of Residual Connections on Learning](https://arxiv.org/pdf/1602.07261.pdf), 2016
+- Franc¸ois Chollet, [Xception: Deep Learning with Depthwise Separable Convolutions](https://arxiv.org/pdf/1610.02357.pdf), 2017
+- Andrew G. Howard et al., [MobileNets: Efficient Convolutional Neural Networks for Mobile Vision Applications](https://arxiv.org/pdf/1704.04861.pdf), 2017
+- Ross Girshick et al, [Rich feature hierarchies for accurate object detection and semantic segmentation](https://arxiv.org/pdf/1311.2524.pdf), 2014
+- Kaiming He et al, [Spatial Pyramid Pooling in Deep Convolutional Networks for Visual Recognition](https://arxiv.org/pdf/1406.4729.pdf), 2015
+- Ross Girshick, [Fast R-CNN](https://arxiv.org/pdf/1504.08083.pdf), 2015
+- Shaoqing Ren et al, [Faster R-CNN: Towards Real-Time Object Detection with Region Proposal Networks](https://arxiv.org/pdf/1506.01497.pdf), 2015
+- Joseph Redmon et al. [You Only Look Once: Unified, Real-Time Object Detection](https://arxiv.org/pdf/1506.02640.pdf), 2016
+- Joseph Redmon et al. [YOLO9000: Better, Faster, Stronger](https://pjreddie.com/media/files/papers/YOLO9000.pdf), 2016
+- Joseph Redmon et al. [YOLOv3: An Incremental Improvement](https://pjreddie.com/media/files/papers/YOLOv3.pdf), 2018
